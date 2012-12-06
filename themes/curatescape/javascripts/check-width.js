f7e9c782ca5bbbccd1783d84c85eb81f4ac4cdcc
Erin Bell <ebellempire@gmail.com>
2012-12-06 13:01:31 -0500

    small/big breakpoint edit

--- themes/curatescape/javascripts/check-width.js
+++ themes/curatescape/javascripts/check-width.js
@@ -1,14 +1,25 @@
 jQuery(document).ready(function() {
     var $window = jQuery(window);
-
+    
         // Function to handle changes to style classes based on window width
+        // Also swaps in thumbnails for larger views where user can utilize Fancybox image viewer
+        
         function checkWidth() {
-        if ($window.width() < 720) {
+        var breakpoint = 705;
+        if ($window.width() < breakpoint) {
             jQuery('body').removeClass('big').addClass('small');
+            jQuery(".item-file img").attr("src", function() { 
+            	return this['src'].replace('square_thumbnails','fullsize'); 
+            	});     
+            jQuery("#item-photos .description , #item-photos .title").show();       
             };
 
-        if ($window.width() >= 720) {
+        if ($window.width() >= breakpoint) {
             jQuery('body').removeClass('small').addClass('big');
+            jQuery(".item-file img").attr("src", function() { 
+            	return this['src'].replace('fullsize','square_thumbnails'); 
+            	});
+            jQuery("#item-photos .description , #item-photos .title").hide();
         }
     }
 
